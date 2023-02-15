7fccc3c44e5f7512aa9ca26ec25079c3d2904620
Marian Zburlea <marian@wowdop.com>
2023-02-15 12:34:18 +0100

    feat: add link to github and from github

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -17,6 +17,18 @@ const Blog = ({ test, articleList }) => (
   <div>
     <h1>Blog</h1>
     <div>Is server side working?</div>
+
+    <h2>Source Code</h2>
+    <p>The source code is publicly available on:</p>
+    <p>
+      <a
+        href="https://github.com/wowdop/blog-next-12-13"
+        target="_blank"
+        rel="noreferrer"
+      >
+        blog-next-12-13
+      </a>
+    </p>
     <p>
       <b>{test}</b>
     </p>
